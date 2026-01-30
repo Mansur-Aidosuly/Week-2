4eface3033c95d77905b8b41b7e5cef972d87545
Mansur <mohchedd888@gmail.com>
2026-01-30 20:45:41 +0500

    Added Boolean Comparison examples

---
+++ Boolean_Comparison.py
@@ -0,0 +1,14 @@
+# Example 1: Equal to
+print(5 == 5)      # True
+
+# Example 2: Not equal to
+print(10 != 3)     # True
+
+# Example 3: Greater than
+print(7 > 10)      # False
+
+# Example 4: Less than or equal
+print(4 <= 4)      # True
+
+# Example 5: Comparing strings
+print("apple" == "banana")  # False
