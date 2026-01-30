7199b1f4a6cb3ce51e893acc92531fcb80e7858f
Mansur <mohchedd888@gmail.com>
2026-01-30 21:22:05 +0500

    Added If Statement examples

---
+++ If_Statement.py
@@ -0,0 +1,25 @@
+# Example 1
+age = 18
+if age >= 18:
+    print("You are an adult")
+
+# Example 2
+is_student = True
+if is_student:
+    print("Student access granted")
+
+# Example 3
+temperature = 30
+if temperature > 25:
+    print("It is hot")
+
+# Example 4
+score = 90
+if score >= 85:
+    print("Excellent score")
+
+# Example 5
+logged_in = True
+if logged_in:
+    print("Welcome back")
+ 
