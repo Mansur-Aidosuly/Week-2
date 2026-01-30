8e5473f77c8cdf525f53579e6026a6e608d6c3e0
Mansur <mohchedd888@gmail.com>
2026-01-30 21:33:26 +0500

    Added If Else statements

---
+++ If_Else.py
@@ -0,0 +1,34 @@
+# Example 1
+age = 16
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+# Example 2
+is_raining = False
+if is_raining:
+    print("Take an umbrella")
+else:
+    print("No umbrella needed")
+
+# Example 3
+password = "admin123"
+if password == "admin123":
+    print("Access granted")
+else:
+    print("Access denied")
+
+# Example 4
+number = 5
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Example 5
+battery = 15
+if battery > 20:
+    print("Battery OK")
+else:
+    print("Low battery")
