ddfa1a3eb22cfcaf5cc335b8868fcb901ae86a75
Mansur <mohchedd888@gmail.com>
2026-01-30 20:32:56 +0500

    first commit

---
+++ Boolean_Value.py
@@ -0,0 +1,20 @@
+# Example 1: Direct boolean value
+is_sunny = True
+print(is_sunny)  # True
+
+# Example 2: Another boolean value
+is_raining = False
+print(is_raining)  # False
+
+# Example 3: Boolean from a condition
+has_homework = True
+print(type(has_homework))  # <class 'bool'>
+
+# Example 4: Boolean assigned manually
+is_student = True
+if is_student:
+    print("You are a student")
+
+# Example 5: Boolean used in logic
+game_over = False
+print("Game running:", not game_over)
