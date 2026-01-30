cd8c16188d1c52d6202c435676c3467213809c58
Mansur <mohchedd888@gmail.com>
2026-01-30 21:37:26 +0500

    Added If Elif Else statements

---
+++ If_Elif_Else.py
@@ -0,0 +1,44 @@
+# Example 1
+score = 75
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+else:
+    print("C")
+
+# Example 2
+temp = 10
+if temp > 25:
+    print("Hot")
+elif temp > 15:
+    print("Warm")
+else:
+    print("Cold")
+
+# Example 3
+age = 14
+if age >= 18:
+    print("Adult")
+elif age >= 13:
+    print("Teen")
+else:
+    print("Child")
+
+# Example 4
+traffic_light = "yellow"
+if traffic_light == "green":
+    print("Go")
+elif traffic_light == "yellow":
+    print("Slow down")
+else:
+    print("Stop")
+
+# Example 5
+balance = 0
+if balance > 0:
+    print("Positive balance")
+elif balance == 0:
+    print("Zero balance")
+else:
+    print("Negative balance")
